ac02b21f97662ed1da53e13222722d9b3f6802db
hardat <hardatnanda@gmail.com>
2026-05-15 11:13:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,25 @@
-self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
+// Med Crush Service Worker
+const CACHE = 'medcrush-v1';
+const ASSETS = ['./','./index.html','./manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
